4b66e4a5ef4a73b064a90cdffbd24395c39e2d22
Mayela3018 <mayela.ticona@tecsup.edu.pe>
2025-03-27 15:51:45 -0500

    ejer1.py

--- labs2/ejer1.py
+++ labs2/ejer1.py
@@ -1,1 +1,11 @@
-#01
+#Problem 1 FACTORIAL
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# using
+if __name__ == "__main__":
+    print(factorial(8))  # Salida esperada: 40320
+

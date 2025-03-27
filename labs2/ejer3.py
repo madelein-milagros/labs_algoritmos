5cfdda192cdfd21ebf5419fe2ee85ee5702c6706
WILSONBAZAN <wilsonbazanlopezaponte@gmail.com>
2025-03-27 17:17:54 -0500

    Update ejer3.py

--- labs2/ejer3.py
+++ labs2/ejer3.py
@@ -1,1 +1,13 @@
+#ejercicio N° 3
 
+def fibonacci(n):
+    if n == 0:
+        return 0  
+    elif n == 1:
+        return 1  
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)  
+
+if __name__ == "__main__":
+    n = 9
+    print(f"El término {n} de Fibonacci es: {fibonacci(n)}")

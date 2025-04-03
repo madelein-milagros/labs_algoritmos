f4a1bd58a78a3980617625ad9ab51db01fe649d8
madelein-milagros <ramoschamorromilagros@gmail.com>
2025-04-02 19:44:32 -0500

    Terminado ejercicio2.py
    Ya esta completo

--- laboratorio3/ejercicio2.py
+++ laboratorio3/ejercicio2.py
@@ -1,1 +1,47 @@
+#ejercicio1
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def list_length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        print(" -> ".join(elements) + " -> None")
+        
+if __name__ == "__main__":
+
+    lista = LinkedList()
+    lista.append(10)
+    lista.append(20)
+    lista.append(30)
+    lista.append(35)
+    print("Lista:")
+    lista.display()  
+    print("Número de nodos:", lista.list_length())  # Output: 4
 
